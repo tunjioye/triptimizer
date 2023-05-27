abe2457a5871cbd2b876814c25c35ddce2d70169
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2023-05-27 09:49:52 -0400

    SETUP : Vercel Redis KV

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  env: {
+    KV_URL: process.env.KV_URL,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
+  },
   publicRuntimeConfig: {
     // Will be available on both server and client
     APP_NAME: process.env.APP_NAME,
