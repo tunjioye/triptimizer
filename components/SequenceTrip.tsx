2da6b73bc1cc9a156c4ce4a5e04e69d269e591fa
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2023-06-30 22:00:03 -0400

    UPDATE : expire saved request after 2 days

--- components/SequenceTrip.tsx
+++ components/SequenceTrip.tsx
@@ -17,6 +17,7 @@ function SequenceTrip() {
   const { request: requestId } = router.query
   const { optimalTrip, optimizeTripBy = 'distance' } = page.use()
   const isViewingResult = requestId && typeof requestId === 'string'
+  const showStartNewTripButton = isViewingResult || optimalTrip != null
 
   const [step, setStep] = useState<number>(1)
   const changeToStep = (newStep: number) => {
@@ -33,16 +34,15 @@ function SequenceTrip() {
         return
       }
       if (optimalTrip.requestId && optimalTrip.optimalTrip) {
-        kv.hset(optimalTrip.requestId, optimalTrip)
-          .then(() => kv.ttl(optimalTrip.requestId))
-          .catch(() => null)
+        // expire in 2 days
+        kv.set(optimalTrip.requestId, optimalTrip, { ex: 60 * 60 * 24 * 2 }).catch(() => null)
       }
     }
   }, [optimalTrip, isViewingResult])
   useEffect(() => {
     if (!router.isReady) return
     if (requestId && typeof requestId === 'string') {
-      kv.hgetall(requestId)
+      kv.get(requestId)
         .then((responseData) => {
           const data = responseData as TripApiResponse['payload']
           if (data && data.requestId === requestId) {
@@ -52,11 +52,11 @@ function SequenceTrip() {
             const optimalTripAddresses = optimalTrips.map((trip) => trip.startAddress)
             // set addresses
             setAddresses(optimalTripAddresses)
+            return
           }
+          toast.error('The result is no longer available. \nWe save results for just 2 days.')
         })
-        .catch(() => {
-          toast.error('Result is no longer available. We store results for 2 days.')
-        })
+        .catch((error) => toast.error(error.message))
     }
   }, [router.isReady, requestId, optimizeTripBy])
 
@@ -64,17 +64,19 @@ function SequenceTrip() {
     <NoSSR>
       <section className={clsx(styles.sequenceTrip)}>
         <div>
-          <details
-            data-step={1}
-            open={step === 1}
-            onClick={(e) => e.preventDefault()}
-            // className={clsx({ disabled: optimalTrip != null })}
-          >
-            <summary onClick={() => changeToStep(1)}>
-              <strong>STEP 1 &middot; Sequence Trip</strong>
-            </summary>
-            <SequenceTripForm showHeading={false} />
-          </details>
+          {!isViewingResult && (
+            <details
+              data-step={1}
+              open={step === 1}
+              onClick={(e) => e.preventDefault()}
+              // className={clsx({ disabled: optimalTrip != null })}
+            >
+              <summary onClick={() => changeToStep(1)}>
+                <strong>STEP 1 &middot; Sequence Trip</strong>
+              </summary>
+              <SequenceTripForm showHeading={false} />
+            </details>
+          )}
 
           <details
             data-step={2}
@@ -83,13 +85,28 @@ function SequenceTrip() {
             className={clsx({ disabled: optimalTrip == null })}
           >
             <summary onClick={() => changeToStep(2)}>
-              <strong>STEP 2 &middot; Sequence Result</strong>
+              {isViewingResult ? (
+                <strong
+                  style={{
+                    display: 'inline-flex',
+                    justifyContent: 'space-between',
+                    gap: '0.25rem',
+                  }}
+                >
+                  <span>Sequence Result </span>
+                  <span className={clsx(styles.startAddressIndicator)}>{requestId}</span>
+                </strong>
+              ) : (
+                <strong>STEP 2 &middot; Sequence Result</strong>
+              )}
             </summary>
             <SequenceTripResult showHeading={false} />
           </details>
         </div>
+
         <div style={{ flex: 1 }} />
-        {optimalTrip != null && (
+
+        {showStartNewTripButton && (
           <div className="hide-on-print">
             <button
               onClick={async () => {
