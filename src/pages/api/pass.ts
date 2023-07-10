73a26650a132045417993300981eeee7e8cb142f
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2023-07-10 15:42:21 -0400

    UPDATE  log sendMail response

--- src/pages/api/pass.ts
+++ src/pages/api/pass.ts
@@ -25,6 +25,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             <p>Created: ${payload.user.created}</p>
           </div>
           `,
+        }).then((res) => {
+          console.log(res)
         })
       }
       res.status(200).json({ payload })
