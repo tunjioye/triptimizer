42d314d90e21c66b70a37e5961f360c74e8e824b
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2023-07-22 09:29:22 -0400

    UPDATE : favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,17 @@ export default function Document() {
           content="Triptimizer is an innovative web application designed to streamline the process of planning and optimizing your trips"
         />
         <meta property="og:url" content="https://test.triptimizer.com" />
-        {/* <meta property="og:image" content="Link preview image URL" /> */}
+        <meta property="og:image" content="https://test.triptimizer.com/triptimizer-1.jpg" />
+        {/* favicons */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#1095c1" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#1095c1" />
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
